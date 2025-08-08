c77e57614f3f89cf2077e486b08e23eac570629a
Jacob Snodgress <jacobsnodgress5@gmail.com>
2025-08-07 17:40:00 -0700

    Update api.py
    updated the api files due to port warning the log was refencing

--- api.py
+++ api.py
@@ -21,6 +21,10 @@ def predict():
                         'Standard Absorption Coefficient' : real_result})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
+import os
+
+port = int(os.environ.get("PORT", 10000))
         
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=port)
